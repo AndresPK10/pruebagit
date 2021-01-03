6544c2367b5823da258e089d9a938aa5a0d3af60
Andres <andresparrado@worldofficeonline.com>
2021-01-02 20:24:39 -0500

    Cambio index se agrega parrafo lorem

---
+++ index.php
@@ -0,0 +1,5 @@
+<html>
+<p>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente animi, sit numquam repellendus tempore facere ullam reprehenderit veniam odio ratione? Soluta officiis, dolore modi placeat totam sit autem optio error!
+</p>
+</html>
